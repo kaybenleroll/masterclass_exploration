552e2f5d9f1433bc784c56faccbebd9e21787371
Mick Cooney <mickcooney@gmail.com>
2018-11-25 14:31:53 +0000

    Committing the first draft of the files

---
+++ install_packages.R
@@ -0,0 +1,13 @@
+pkg_list <- c('rmarkdown'
+             ,'knitr'
+             ,'tidyverse'
+             ,'scales'
+             ,'GGally'
+             ,'Rtsne'
+             ,'feather'
+             ,'cowplot'
+             ,'DataExplorer'
+)
+
+
+install.packages(pkg_list, dependencies = TRUE)
